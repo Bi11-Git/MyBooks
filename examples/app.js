1c48c131662319a214bde59a1c74d0810821b165
Vasilis Hyskaj <Anxheldohyskaj@gmail.com>
2021-05-14 15:36:05 +0300

    example finish

--- examples/app.js
+++ examples/app.js
@@ -1,32 +1,67 @@
-const data = require('./data.js');
 const express = require('express');
 const app = express();
-
 const parser = require('body-parser');
+
 const cors = require('cors');
-app.use(cors({
-    origin: '*'
-}));
 
-app.get('/listUsers', (req, res) => {
-    res.send(JSON.stringify(data));
+
+
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('users.sqlite');
+
+app.use(cors());
+app.use(parser.json());
+
+app.get('/listUsers', async (req, res) => {
+    const q = 'SELECT * FROM users';
+    try {
+        const rows = await query(q);
+        res.send(JSON.stringify(rows));
+    } catch(err) {
+        res.send('Error executing query')
+    }
 });
 
-app.get('/:id', (req, res) => {
-    
+app.get('/user/:id', async (req, res) => {
+    const q = 'SELECT * FROM users where rowid='+ req.params.id + ' ;';
+    try {
+        const rows = await query(q);
+        res.send(JSON.stringify(rows));
+    } catch(err) {
+        res.send('Error executing query')
+    };
 });
 
-app.post('/', (req, res) => {
-    console.log(req.body);
-    console.log('request!!!');
+app.post('/addUser', (req, res) => {
+    console.log('not implemented yet');
     const user = req.body;
-    data.push(user);
-    res.send('All ok');
+    const q = `insert into users ( "name", "password", "profession") values ( "` + user.name + `", "` + user.password +`", "` + user.profession + `"); `;
+    
+    console.log(q);
+    db.run( q, ( err) => {
+        if( err) {
+            res.send('Error executing query')
+        } else {
+            res.send('All ok');
+        }
+    });
 });
 
 app.delete('/deleteUser', (req, res) => {
     
 });
 
-app.listen('3000');
-console.log('Server is running on port 3000');
+function query(q) {
+    return new Promise( ( resolve, reject) => {
+        db.all(q, ( err, rows) => {
+            if( err) {
+                reject(err);
+            } else {
+                resolve(rows);
+            }
+        })
+    });
+}
+
+app.listen('2020');
+console.log('Server is running on port 2020');
