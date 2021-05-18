84103e6b4ef4640f7ae7df93bd9321b1816ad413
Vasilis Hyskaj <Anxheldohyskaj@gmail.com>
2021-05-18 15:01:21 +0300

    finished

--- controller.js
+++ controller.js
@@ -14,15 +14,28 @@ class Book {
 window.addEventListener('load', (event) => {
     document.getElementById("addBook").addEventListener('click', (event) => {
 
+        document.getElementById('price').setCustomValidity('');
+
         const author = document.getElementById('author').value;
         const title = document.getElementById('title').value;
         const genre = document.getElementById('genre').value;
         const price = document.getElementById('price').value;
 
+        if(isNaN(price)) {
+
+            document.getElementById('price').setCustomValidity('Not a number!');
+
+            return;
+        }
+
+        if (author == "" || title == "" || genre == "" || price == "") {
+            return;
+        }
+
         const book = new Book(0, author, title, genre, price);
 
         const xhr = new XMLHttpRequest();
-        xhr.open('POST', 'http://localhost:2020', true);
+        xhr.open('POST', 'http://localhost:2020/books/', true);
         xhr.setRequestHeader('Content-Type', 'application/json');
         xhr.send(JSON.stringify(book));
         xhr.onreadystatechange = function () {
@@ -33,6 +46,8 @@ window.addEventListener('load', (event) => {
             }
         }
 
+        return false;
+
     });
 
 });
@@ -42,38 +57,34 @@ window.addEventListener('load', (event) => {
 
         const key = document.getElementById('key').value;
 
-        if(key == "") {
+        if (key == "") {
             return;
         }
 
+        var table = document.getElementById("table");
+        //or use :  var table = document.all.tableid;
+
+        for (var i = table.rows.length - 1; i > 0; i--) {
+            table.deleteRow(i);
+        }
+
         const xhr = new XMLHttpRequest();
-        xhr.open('GET', 'http://localhost:2020/' + key, true);
+        xhr.open('GET', 'http://localhost:2020/books/' + key, true);
         xhr.send();
         xhr.onreadystatechange = function () {
             if (xhr.readyState == 4) {
                 if (xhr.status == 200) {
-                    const table = document.getElementById('table');
-                    const results = JSON.parse(xhr.responseText);
-                    const item = results[0];
 
-                    if (item === undefined) {
+                    var results;
+                    try {
+                        results = JSON.parse(xhr.responseText);
+                    } catch (err) {
                         alert('This book doesnt exist!');
-                    } else {
+                        return;
+                    }
 
-                        const row = table.insertRow();
-
-                        const idCell = row.insertCell(0);
-                        const authorCell = row.insertCell(1);
-                        const titleCell = row.insertCell(2);
-                        const genreCell = row.insertCell(3);
-                        const priceCell = row.insertCell(3);
-
-                        idCell.innerHTML = item.id;
-                        authorCell.innerHTML = item.author;
-                        titleCell.innerHTML = item.title;
-                        genreCell.innerHTML = item.genre;
-                        priceCell.innerHTML = item.price;
-
+                    for (var i = 0; i < results.length; i++) {
+                        print(results[i]);
                     }
 
                 }
@@ -83,3 +94,29 @@ window.addEventListener('load', (event) => {
     });
 
 });
+
+function print(item) {
+
+    if (item === undefined) {
+        alert('aaaaaa');
+        return;
+    }
+
+
+    const table = document.getElementById('table');
+
+    const row = table.insertRow();
+
+    const idCell = row.insertCell(0);
+    const authorCell = row.insertCell(1);
+    const titleCell = row.insertCell(2);
+    const genreCell = row.insertCell(3);
+    const priceCell = row.insertCell(3);
+
+    idCell.innerHTML = item.id;
+    authorCell.innerHTML = item.author;
+    titleCell.innerHTML = item.title;
+    genreCell.innerHTML = item.genre;
+    priceCell.innerHTML = item.price;
+
+}
--- server.js
+++ server.js
@@ -14,46 +14,116 @@ const db = new sqlite3.Database(databaseURL);
 server.use(cors());
 server.use(parser.json());
 
-server.get('/:search', (req, res) => {
+server.post('/books/', async(req, res) => {
+    const book = req.body;
+    const q = `insert into books ( "author", "title", "genre", "price") values ( "` + book.author + `", "` + book.title + `", "` + book.genre + `", "` + book.price + `"); `;
+    const key = book.title;
+
+    try {
+    
+        const rows = await query( 'SELECT * FROM books WHERE title="' + key + '" ;' );
+
+        //check if the book title already exist in database
+        if (rows.length === 0) {
+
+            db.run(q, (err) => {
+                if (err) {
+                    conslole.log(err);
+                    res.send('Error executing query')
+                } else {
+                    res.send('book added succesfully!');
+                }
+            });
+
+        } else {
+            res.send('This book already exist!');
+        }
+    } catch (err) {
+        res.send('Error executing query')
+    };
+});
+
+server.get('/books/:search', async(req, res) => {
 
     const key = req.params.search;
 
-    const q = 'SELECT * FROM books WHERE author="' + key +'" or title="' + key + '" or genre="' + key + '" ;';
-    db.all(q, ( err, rows) => {
-        if( err) {
-            console.log(err);
+    // the search method return 2 dimensional array 
+    var books = await search(key);
+
+    // convert to 1 dimensional 
+    for( var i = 0; i < books.length; i++) {
+        books[i] = books[i][0];
+    }
+
+    try {
+        
+        if (books == 0) {
+            res.send('This book dooes not exist!');
         } else {
-            res.send(JSON.stringify(rows));
+            res.send(JSON.stringify(books));
         }
-    })
+    } catch (err) {
+        res.send('Error executing query')
+    };
 
 });
 
-server.get('/', (req, res) => {
+function query(q) {
 
-    const q = 'SELECT * FROM books;';
-    db.all(q, ( err, rows) => {
-        if( err) {
-            console.log(err);
-        } else {
-            res.send(JSON.stringify(rows));
-        }
-    })
-
-});
-
-server.post('/', (req, res) => {
-    const book = req.body;
-    const q = `insert into books ( "author", "title", "genre", "price") values ( "` + book.author + `", "` + book.title + `", "` + book.genre +`", "` + book.price + `"); `;
-    console.log(q);
-    db.run( q, ( err) => {
-        if( err) {
-            res.send('Error executing query')
-        } else {
-            res.send('book added succesfully!');
-        }
+    return new Promise((resolve, reject) => {
+        db.all(q, (err, rows) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(rows);
+            }
+        })
     });
-});
+}
+
+async function search(key) {
+
+    const row = await query('SELECT * FROM books ;');
+
+    if (row === undefined) {
+        return 0;
+    }
+
+    //convert all books to text
+    var text = JSON.stringify(row);
+
+    //convert text to array
+    text = text.split('{');
+
+    var books = new Array();
+
+    for (var i = 0; i < text.length; i++) {
+
+        
+        text[i] = text[i].toUpperCase();
+
+        //search if the books one by one if they includes the key
+        if (text[i].includes(key.toUpperCase())) {
+
+            try {
+
+                // the index of book on the array is the id on the database
+                // then retrieve this book from the db
+                const book = await query('SELECT * FROM books WHERE id="' + i + '" ');
+                books.push(book);
+
+            } catch (err) {
+                console.log(err);
+            }
+        }
+
+    }
+
+    return books;
+
+}
+
+
 
 server.listen(2020);
 
